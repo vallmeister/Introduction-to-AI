7241fddffb91f3be18aded7345d763856eb182e6
Christian V <hans.mull100@gmail.com>
2021-01-20 15:44:21 +0100

    Implemented game tree build from top to down :art:

--- src/games/Euclid.java
+++ src/games/Euclid.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Euclid {
   private static int largerNumber;
   private static int smallerNumber;
+  private static Tree gameTree;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     readInput();
-    Node root = new Node(largerNumber, smallerNumber, null);
-
+    buildTree();
+    printOutput();
   }
 
   private static void readInput() {
@@ -27,6 +29,34 @@ public class Euclid {
       e.printStackTrace();
     }
   }
+
+  private static void buildTree() throws InterruptedException {
+    Node root = new Node(largerNumber, smallerNumber, null, true);
+    gameTree = new Tree(root);
+
+    LinkedBlockingQueue<Node> nodeQueue = new LinkedBlockingQueue<>();
+    nodeQueue.put(root);
+    while (nodeQueue.size() > 0) {
+      Node currentNode = nodeQueue.poll();
+      int larger = currentNode.largerNumber;
+      int smaller = currentNode.smallerNumber;
+      int div = larger / smaller;
+      for (int i = 1; i <= div; i++) {
+        Node temp = new Node(larger - i * smaller, smaller, currentNode, !currentNode.minimax);
+        currentNode.successors.add(temp);
+        // TODO: goal state test
+        nodeQueue.put(temp);
+      }
+    }
+  }
+
+  private static void printOutput() {
+    if (gameTree.root.gameValue == 1) {
+      System.out.println("Stan wins");
+    } else {
+      System.out.println("Ollie wins");
+    }
+  }
 }
 
 class Node {
@@ -35,10 +65,24 @@ class Node {
   public List<Node> successors;
   public Node predecessor;
 
-  public Node(int first, int second, Node parent) {
+  // 0 represents min-player, 1 is for max-player
+  public boolean minimax;
+
+  public int gameValue;
+
+  public Node(int first, int second, Node parent, boolean player) {
     largerNumber = Math.max(first, second);
     smallerNumber = Math.min(first, second);
     successors = new ArrayList<>();
     predecessor = parent;
+    minimax = player;
+  }
+}
+
+class Tree {
+  public final Node root;
+
+  public Tree(Node node) {
+    root = node;
   }
 }
